790c6dd3d7b07e7260a8857b92deb9dd3901cb03
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-06-24 11:58:12 +0100

    Upgrade

---
+++ web-assembly/target/debug/deps/web_assembly.d
@@ -0,0 +1,5 @@
+C:\Users\maimm\ALL-PROJECTS\Research Papper\web-assembly\target\debug\deps\web_assembly.exe: src\main.rs
+
+C:\Users\maimm\ALL-PROJECTS\Research Papper\web-assembly\target\debug\deps\web_assembly.d: src\main.rs
+
+src\main.rs:
