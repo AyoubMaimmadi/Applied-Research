88bdadbe2e3675b99769345097c190dc1ba5745f
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-06-28 13:33:49 +0100

    Main App

--- react/src/App.tsx
+++ react/src/App.tsx
@@ -1,8 +1,39 @@
 import React from 'react'
-import './App.css'
 
-function App() {
-  return <div>Hello</div>
+import { useEffect, useState } from 'react'
+import './App.css'
+import Item from './item/Item'
+
+function App({
+  size,
+  depth,
+  intervalMs,
+}: {
+  depth: number
+  size: number
+  intervalMs: number
+}) {
+  const [renderCount, render] = useState(0)
+
+  useEffect(
+    function () {
+      const id = setTimeout(function () {
+        render(renderCount + 1)
+      }, intervalMs)
+      return function () {
+        clearTimeout(id)
+      }
+    },
+    [intervalMs, renderCount]
+  )
+
+  const items = new Array(32)
+    .fill(0)
+    .map((_, i) => (
+      <Item girth={size} key={i} bit={i} depth={depth} value={renderCount} />
+    ))
+
+  return <>{items}</>
 }
 
 export default App
