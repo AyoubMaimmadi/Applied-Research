66f08b57c60aeef5bab2754252b4355d6f08fa5c
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-06-29 13:30:50 +0100

    new

--- react/server.js
+++ react/server.js
@@ -4,23 +4,30 @@ var path = require('path')
 var fs = require('fs')
 var express = require('express')
 var main_server_1 = require('./dist/server/main-server')
+
 console.log(main_server_1['default'])
+
 var app = express()
+
 function resolve(p) {
   return path.resolve(__dirname, p)
 }
+
 app.use(
   require('serve-static')(resolve('dist/client'), {
     index: false,
   })
 )
+
 var index = fs
   .readFileSync(path.join(__dirname, 'dist', 'client', 'index.html'))
   .toString()
+
 function getMicroTime() {
   var hrTime = process.hrtime()
   return Math.round(hrTime[0] * 1000000 + hrTime[1] / 1000)
 }
+
 app.use('/:size/:depth', function (req, res) {
   var now = getMicroTime()
   var size = +req.params.size
@@ -35,6 +42,7 @@ app.use('/:size/:depth', function (req, res) {
     .set({ 'Content-Type': 'text/html' })
     .end(html)
 })
+
 app.listen(3001, function () {
   console.log('http://localhost:3001')
 })
